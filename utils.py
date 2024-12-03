238e5600457759c3d245ce5e25603a3a76118c60
lorenzoorsi <lorenzo.orsingher@gmail.com>
2024-12-03 17:45:23 +0100

    add function to fix seed

--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 import datetime
+import random
+import numpy as np
 import timm
 import torch
 import argparse
@@ -53,6 +55,14 @@ def gen_run_name(config=None):
     else:
         run_name = f"run_{timestamp}"
     return run_name
+
+
+def set_seed(seed):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.backends.cudnn.deterministic = True
 
 
 def get_args():
