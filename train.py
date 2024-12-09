c994afa85ff34483831bae2f2b6d9de69e5317b9
lorenzoorsi <lorenzo.orsingher@gmail.com>
2024-12-09 21:57:51 +0100

    fix train scheduler

--- train.py
+++ train.py
@@ -175,8 +175,8 @@ if __name__ == "__main__":
                 model, forget_loader, criterion, DEVICE
             )
 
-        # scheduler.step(val_top1)
-        scheduler.step()
+        scheduler.step(val_top1)
+        # scheduler.step()
 
         print(f"lr: {optimizer.param_groups[0]['lr']}")
 
